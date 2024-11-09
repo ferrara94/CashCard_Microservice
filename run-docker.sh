72f27982b38ed0279c7b75e78d482ae1d1359aa7
ferrara94 <ing.ferrara94@gmail.com>
2024-11-09 22:24:09 +0100

    server port re-stored Server starting port changed

--- run-docker.sh
+++ run-docker.sh
@@ -4,4 +4,4 @@ echo "Building the Docker image for Cashcard..."
 docker build -t cashcard_docker -f Docker/Dockerfile .
 
 echo "Running the Docker container..."
-docker run -p 8080:8080 cashcard_docker
+docker run -p 8081:8081 cashcard_docker
